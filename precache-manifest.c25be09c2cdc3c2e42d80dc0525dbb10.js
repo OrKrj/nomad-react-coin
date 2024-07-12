dd1e5054f1a0ee968fdaa7f23f32dd7c3db860e2
Zun <k5orkrj@gmail.com>
2024-07-13 07:05:35 +0900

    Updates

---
+++ precache-manifest.c25be09c2cdc3c2e42d80dc0525dbb10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf8b9391234ce9f26181dff39823f62",
+    "url": "/nomad-react-coin/index.html"
+  },
+  {
+    "revision": "fc9f2f110284dc73a341",
+    "url": "/nomad-react-coin/static/js/2.bb578d8f.chunk.js"
+  },
+  {
+    "revision": "2f58089c61d46f50cf6fed08f3e9f0eb",
+    "url": "/nomad-react-coin/static/js/2.bb578d8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01b545b37a77238e5f13",
+    "url": "/nomad-react-coin/static/js/main.2feeb339.chunk.js"
+  },
+  {
+    "revision": "993d629a972f661ca65b",
+    "url": "/nomad-react-coin/static/js/runtime-main.02456c07.js"
+  }
+]);
